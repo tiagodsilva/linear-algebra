e80fec824e05f99e5d4db458add168b0943dc479
Tiago <tdsh97@gmail.com>
2024-03-18 10:46:29 -0300

    update

--- hw1/ex6.py
+++ hw1/ex6.py
@@ -5,7 +5,7 @@ import tqdm
 sys.path.append('.') 
 from mpl_preamble import * 
 
-TRIALS = 200 
+TRIALS = 500
 device = 'cuda' if torch.cuda.is_available() else 'cpu' 
 
 if __name__ == '__main__':
@@ -15,13 +15,17 @@ if __name__ == '__main__':
         for trial in tqdm.trange(TRIALS): 
             m = torch.randn((size, size), device=device) 
             y = torch.randn((size,), device=device) 
-            s = time.time() 
+            s = time.time()
             torch.linalg.solve(m, y) 
             runtimes[idx, trial] = time.time() - s 
-    avg = torch.log(runtimes).mean(axis=1).cpu().numpy()
+    avg = torch.log(runtimes).median(axis=1).values.cpu().numpy()
     std = torch.log(runtimes).std(axis=1).cpu().numpy() 
     log_sizes = torch.log(sizes).cpu().numpy() 
-    linreg, _, _, _ = np.linalg.lstsq(log_sizes.reshape(-1, 1), avg.reshape(-1, 1))
+    linreg, _, _, _ = np.linalg.lstsq(
+            np.hstack([
+                np.ones_like(log_sizes).reshape(-1, 1), 
+                log_sizes.reshape(-1, 1)
+            ]), avg.reshape(-1, 1))
     print(linreg.squeeze()) 
     plt.plot(log_sizes, avg) 
     plt.fill_between(log_sizes, avg-std, avg+std, alpha=.5) 
